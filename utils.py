5abbefc1c28b7094e3afa097f49e0252886c7d46
DocyNoah_l <yoweifc_l@gmail.com>
2021-07-16 21:12:15 +0900

    Add current_time()

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+import time
 
 
 def print_shape(*tensors):
@@ -13,3 +14,21 @@ def print_shape(*tensors):
             arg_names = w.group(1).split(", ")
     for idx, arg_name in enumerate(arg_names):
         print("# {0}.shape: {1}".format(arg_name, tensors[idx].shape))
+
+
+# ex) yy-mm-dd/hh:mm:ss
+def print_value(value):
+    this_function_name = inspect.currentframe().f_code.co_name
+    lcls = inspect.stack()
+    outer = re.compile("\((.+)\)")
+    arg_names = None
+    for lcl in lcls:
+        if lcl.code_context[0].strip().startswith(this_function_name):
+            w = outer.search(lcl.code_context[0].split(this_function_name)[1])
+            arg_names = w.group(1).split(", ")
+    for idx, arg_name in enumerate(arg_names):
+        print("# {0} : {1}".format(arg_name, value))
+
+
+def current_time():
+    return time.strftime('%y-%m-%d/%X', time.localtime(time.time()))
