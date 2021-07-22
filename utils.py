43cd7140fcaef254738e134b01d5840e41b1e3fd
DocyNoah_l <yoweifc_l@gmail.com>
2021-07-22 18:27:09 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,26 +9,26 @@ def print_shape(*tensors):
     outer = re.compile("\((.+)\)")
     arg_names = None
     for lcl in lcls:
-        if lcl.code_context[0].strip().startswith(this_function_name):
+        if this_function_name in lcl.code_context[0].strip():
             w = outer.search(lcl.code_context[0].split(this_function_name)[1])
             arg_names = w.group(1).split(", ")
     for idx, arg_name in enumerate(arg_names):
         print("# {0}.shape: {1}".format(arg_name, tensors[idx].shape))
 
 
-# ex) yy-mm-dd/hh:mm:ss
 def print_value(value):
     this_function_name = inspect.currentframe().f_code.co_name
     lcls = inspect.stack()
     outer = re.compile("\((.+)\)")
     arg_names = None
     for lcl in lcls:
-        if lcl.code_context[0].strip().startswith(this_function_name):
+        if this_function_name in lcl.code_context[0].strip():
             w = outer.search(lcl.code_context[0].split(this_function_name)[1])
             arg_names = w.group(1).split(", ")
     for idx, arg_name in enumerate(arg_names):
         print("# {0} : {1}".format(arg_name, value))
 
 
+# ex) yy-mm-dd/hh:mm:ss
 def current_time():
     return time.strftime('%y-%m-%d/%X', time.localtime(time.time()))
